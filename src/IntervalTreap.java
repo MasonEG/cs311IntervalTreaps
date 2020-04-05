7557bd38c4abb3fd0b7ba81fde771030d0a69a54
Mason Gil <masonegil@gmail.com>
2020-04-04 23:45:21 -0500

    Created Interval database and changed IntervalNode to Interval

--- src/IntervalTreap.java
+++ src/IntervalTreap.java
@@ -1,4 +1,18 @@
-public class IntervalTreap {
+public class IntervalDatabase {
+
+    public Interval[] intervalNodes;
+
+    public Interval intervalSearch (int h, int l) {
+
+    }
+
+    public void intervalInsert (Interval x) {
+
+    }
+
+    public void intervalDelete (Interval x) {
+
+    }
 
 
 }
