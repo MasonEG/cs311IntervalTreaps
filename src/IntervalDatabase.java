abd9723bd288021738887d05b00d7c16f20ba854
Andrei Baechle <abaechle@iastaet.edu>
2020-04-05 00:07:36 -0500

    changed class names

--- src/IntervalDatabase.java
+++ src/IntervalDatabase.java
@@ -9,10 +9,10 @@ public class IntervalDatabase {
     public Interval intervalSearch (Interval i) {
         IntervalNode curr = this.root;
         while(curr != null && curr.left.imax >= i.low){
-            if(curr.left != null && curr.left.imax >= i.low)
-                curr = curr.left;
-            else
-                curr = curr.left;
+//            if(curr.left != null && curr.left.imax >= i.low)
+//                curr = curr.left;
+//            else
+//                curr = curr.left;
         }
         return curr;
     }
